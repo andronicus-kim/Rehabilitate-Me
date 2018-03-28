8582a8ff1fb5b289fb6663fb0bfc8ec112cc60c3
andronicus-kim <androkim.kim7@gmail.com>
2018-03-28 22:21:52 +0300

    sets up the project and sign in activity

---
+++ app/src/main/java/com/andronicus/rehabilitateme/SignupActivity.java
@@ -0,0 +1,19 @@
+package com.andronicus.rehabilitateme;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignupActivity extends AppCompatActivity {
+
+    public Intent newIntent(@NonNull Context context){
+        return new Intent(context,SignupActivity.class);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
