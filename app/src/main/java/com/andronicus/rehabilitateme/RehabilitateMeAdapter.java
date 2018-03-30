b14e3a946dc90ea72070c10c80d55d9a65b12339
andronicus-kim <androkim.kim7@gmail.com>
2018-03-30 23:37:14 +0300

    creates adapter

---
+++ app/src/main/java/com/andronicus/rehabilitateme/RehabilitateMeAdapter.java
@@ -0,0 +1,59 @@
+package com.andronicus.rehabilitateme;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by andronicus on 3/30/2018.
+ */
+
+public class RehabilitateMeAdapter extends RecyclerView.Adapter<RehabilitateMeAdapter.RehabilitateMeViewHolder> {
+
+    private List<User> mUsers;
+
+    public RehabilitateMeAdapter (@NonNull List<User> users){
+        this.mUsers = users;
+    }
+    @Override
+    public RehabilitateMeViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_rehab_info,parent,false);
+        return new RehabilitateMeViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RehabilitateMeViewHolder holder, int position) {
+        holder.bind(mUsers.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUsers.size();
+    }
+
+    public class RehabilitateMeViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView mName;
+        private TextView mAge;
+        private TextView mAddiction;
+
+        public RehabilitateMeViewHolder(View view) {
+            super(view);
+            mName = view.findViewById(R.id.tv_name);
+            mAge = view.findViewById(R.id.tv_age);
+            mAddiction = view.findViewById(R.id.tv_addiction);
+        }
+
+        public void bind(User user){
+            mName.setText(user.getName());
+            mAge.setText(user.getAge());
+            mAddiction.setText(user.getAddiction());
+        }
+    }
+}
---
+++ app/src/main/java/com/andronicus/rehabilitateme/User.java
@@ -0,0 +1,36 @@
+package com.andronicus.rehabilitateme;
+
+/**
+ * Created by andronicus on 3/30/2018.
+ */
+
+public class User {
+
+    private String name;
+    private int age;
+    private String addiction;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddiction() {
+        return addiction;
+    }
+
+    public void setAddiction(String addiction) {
+        this.addiction = addiction;
+    }
+}
